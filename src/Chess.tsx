b39d68d4818e97baded2afa070d1b7a64097c309
Austin Weeks <168379071+austin-weeks@users.noreply.github.com>
2024-06-09 17:22:13 -0500

    bug fixes

--- src/Chess.tsx
+++ src/Chess.tsx
@@ -288,8 +288,8 @@ interface CapturedPiecesProps {
     isWhite: boolean
 }
 function CapturedPieces({pieces, isWhite}: CapturedPiecesProps) {
-    if (pieces.length === 1) return (<></>);
-    
+    if (pieces.length === 0) return (<></>);
+
     return (
         <div className="captured-pieces" style={isWhite ? {} : {justifyContent: "flex-end"}}>
             <div className={isWhite ? "captured-inner" : "captured-inner inner-reverse"}>
